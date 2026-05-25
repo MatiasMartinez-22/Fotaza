73e9204bf8d97c68b5d0081483369d4a8eafb792
MatiasMartinez-22 <matias.e.martinez1993@gmail.com>
2026-05-24 23:44:32 -0300

    probando

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const mainRoutes = require('./routes/mainRoutes');
+
+// Configuración
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+app.use('/', mainRoutes);
+
+// Puerto
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
