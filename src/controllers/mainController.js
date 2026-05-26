1c36c46e2d518ab134636c762afa1ff0565c7775
MatiasMartinez-22 <matias.e.martinez1993@gmail.com>
2026-05-26 20:32:52 -0300

    conectar postgres

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,29 @@
+/*
 function index(req, res) {
 
     res.render('index');
+}
+
+module.exports = {
+    index
+};*/
+
+const pool = require('../config/db');
+
+async function index(req, res) {
+
+    try {
+
+        const result = await pool.query('SELECT NOW()');
+
+        console.log(result.rows);
+
+        res.render('index');
+
+    } catch (error) {
+
+        console.log(error);
+    }
 }
 
 module.exports = {
